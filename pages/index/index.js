a13b12ecfbaa7ac00f021ff2c2ba70f4e45cc855
Michael <yiwangchunyu@163.com>
2017-11-29 02:25:58 +0800

    a demo of index

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,53 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
+var utils = require('../../utils/util.js')
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    swiper_url: [
+      '../../images/index/swiper/1.jpg',
+      '../../images/index/swiper/2.jpg',
+      '../../images/index/swiper/3.jpg',
+      '../../images/index/swiper/4.jpg'
+    ],
+    list: [{ header: 'lost&found信息' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }, { text: '物品描述', image: '../../images/index/goods/eg.jpg' }],
+    duration: 2000,
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    loading: false,
+    plain: false
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  bindViewTap: function (e) {
+    
+  },
+
+  loadMore: function (e) {
+    
+  },
+  getNextDate: function () {
+    var now = new Date()
+    now.setDate(now.getDate() - this.index++)
+    return now
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+    var that = this
+    for(var i=0;i<10;i++)
+    list.concat({text: '物品描述',image: '../../images/index/goods/eg.jpg'})
+
+    wx.request(
+     
+    )
+    this.index = 1
+    //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function(userInfo){
+    //   //更新数据
+    //   that.setData({
+    //     userInfo:userInfo
+    //   })
+    // })
+
   }
 })
